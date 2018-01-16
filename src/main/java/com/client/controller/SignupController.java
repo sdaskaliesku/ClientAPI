f187b9f16604e3f7fc51dea34942be0055f0661f
Serhii Daskaliesku <manson.serg@gmail.com>
2018-01-16 03:57:28 +0200

    Enabled signup

--- src/main/java/com/client/controller/SignupController.java
+++ src/main/java/com/client/controller/SignupController.java
@@ -1,42 +1,50 @@
 package com.client.controller;
 
-import javax.validation.Valid;
-
-import com.client.domain.db.Account;
 import com.client.domain.SignupRequest;
+import com.client.domain.db.Account;
 import com.client.service.UserService;
+import com.client.support.web.MessageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.client.support.web.*;
+import javax.validation.Valid;
 
-//@Controller
+@Controller
 public class SignupController {
 
-    /*private static final String SIGNUP_VIEW_NAME = "signup/signup";
+    private static final String SIGNUP_VIEW_NAME = "signup/signup";
+    private static final boolean SIGN_UP_ENABLED = true;
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@RequestMapping(value = "signup")
-	public String signup(Model model) {
-		model.addAttribute(new SignupRequest());
+    @RequestMapping(value = "signup")
+    public String signup(Model model) {
+        if (!SIGN_UP_ENABLED) {
+            return null;
+        }
+        model.addAttribute(new SignupRequest());
         return SIGNUP_VIEW_NAME;
-	}
+    }
 
-	@RequestMapping(value = "signup", method = RequestMethod.POST)
-	public String signup(@Valid @ModelAttribute SignupRequest signupRequest, Errors errors, RedirectAttributes ra) {
-		if (errors.hasErrors()) {
-			return SIGNUP_VIEW_NAME;
-		}
-		Account account = userService.create(signupRequest.createAccount());
-		userService.signin(account);
+    @RequestMapping(value = "signup", method = RequestMethod.POST)
+    public String signup(@Valid @ModelAttribute SignupRequest signupRequest, Errors errors, RedirectAttributes ra) {
+        if (!SIGN_UP_ENABLED) {
+            return null;
+        }
+        if (errors.hasErrors()) {
+            return SIGNUP_VIEW_NAME;
+        }
+        Account account = userService.create(signupRequest.createAccount());
+        userService.signin(account);
         // see /WEB-INF/i18n/messages.properties and /WEB-INF/views/homeSignedIn.html
         MessageHelper.addSuccessAttribute(ra, "signup.success");
-		return "redirect:/";
-	}*/
+        return "redirect:/";
+    }
 }
